bbf14e4f0c456c72f4b70348d9f2bd6aa87899dc
Mikhail <cherezov1601@gmail.com>
2024-03-17 18:19:55 +0500

    base game for developing a new interface.

---
+++ app.py
@@ -0,0 +1,59 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class TicTacToe:
+
+    def __init__(self, game_board):
+        self.game_board = game_board  # создание игрового окна
+        self.game_board.title("Крестики Нолики")
+        self.current_player = "X"  # установка начального игрока
+        self.board = [" " for _ in range(9)]  # создание пустого игрового поля
+        self.buttons = []  # список для хранения кнопок
+
+        for i in range(3):
+            row = []  # список кнопок
+            for j in range(3):
+                button = tk.Button(self.game_board, text=" ",
+                                   font=("Arial", 20),
+                                   height=2,
+                                   width=5,
+                                   command=lambda i=i, j=j: self.on_button_click(i, j))
+                button.grid(row=i, column=j, sticky="nsew")
+                row.append(button)
+            self.buttons.append(row)
+
+        self.reset_button = tk.Button(self.game_board,
+                                      text="Новая Игра",
+                                      command = self.reset_game)
+        self.reset_button.grid(row=3, column=0, columnspan=3, sticky="nsew")
+
+    def on_button_click(self, i, j):
+        if self.board[i*3+j] == " ":  # Проверяем пустая ли клетка.
+            self.board[i*3+j] = self.current_player  # Устанавливаем знак текущего игрока.
+            self.buttons[i][j].config(text=self.current_player)  # Обновляем клетку на знак текущего игрока.
+            if self.check_winner(i,j):
+                messagebox.showinfo("Победа!", f"Победи игрок '{self.current_player}' ")
+                self.reset_game()
+            elif " " not in self.board:
+                messagebox.showinfo("Победа!", f"Ничья!")
+                self.reset_game()
+            else:
+                self.current_player = "O" if self.current_player == "X" else "X"  # Смена текущего игрока
+
+    def check_winner(self, i, j):
+        row = all(self.board[i * 3 + col] == self.current_player for col in range(3))
+        col = all(self.board[row * 3 + j] == self.current_player for row in range(3))
+        diag1 = all(self.board[i * 3 + i] == self.current_player for i in range(3))
+        diag2 = all(self.board[i * 3 + 2 - i] == self.current_player for i in range(3))
+        return any([row, col, diag1, diag2])
+
+    def reset_game(self):
+        self.current_player = "X"
+        self.board = [" " for _ in range(9)]
+        for i in range(3):
+            for j in range(3):
+                self.buttons[i][j].config(text='')
+
+root = tk.Tk()
+game = TicTacToe(root)
+root.mainloop()
